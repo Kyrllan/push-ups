143ecbc8eb6df6758051d9446dbab1dcb2a4b7e5
Kyrllan <kyrllan@hotmail.com>
2024-10-14 18:27:08 -0300

    pwa app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["~/assets/css/base.css"],
+  modules: ["@vite-pwa/nuxt"],
+  pwa: {
+    manifest: {
+      name: "Push-ups",
+      short_name: "Push-ups",
+      description: "Push-ups app",
+      lang: "pt-BR",
+      display: "standalone",
+      start_url: "/",
+      theme_color: "#ffffff",
+      background_color: "#ffffff",
+      icons: [
+        {
+          src: "/icon-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "/icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+  },
 });
